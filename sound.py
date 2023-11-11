6981c02bcaee9007cf3da67beb77c87c6f8faa73
Conrad Heinrich Carl <coheca0815@gmail.com>
2023-11-11 22:33:55 +0100

    add config for beep volume

--- sound.py
+++ sound.py
@@ -1,9 +1,12 @@
 from pydub import AudioSegment
 from pydub.playback import play
-
+import configparser
 
 def play_beep():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
     noise = AudioSegment.from_wav("./assets/beep.wav")
+    noise = noise + config["Assistant"]["volume"]
     print('beep')
     play(noise)
 
