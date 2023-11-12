a31c2b3501707684009dac1c74544f4a26d54946
Conrad Heinrich Carl <coheca0815@gmail.com>
2023-11-12 22:14:59 +0100

    refactor some more app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from recognizer import Recognizer
 from sound import play_beep
 from speak import speak
 from tools.set_light_status import SET_LIGHT_STATUS
+from cmd_parser import parse_cmd
 
 class App:
     def __init__(self, config_path: str = "config.ini"):
@@ -13,22 +14,33 @@ class App:
         self.rec = Recognizer()
         self.tool = SET_LIGHT_STATUS
         self.c_queue = ContextQueue()
+        self.answer = bool(self.config["Assistant"]["Answer"] or False)
+        self.name = self.config["Assistant"]["Name"] or "bob"
+
+
+    def get_prompt(self) -> str:
+        promt = self.rec.listen()["transcription"]
+        print(f"Prompt: {promt}")
+
+    def wait_for_activation(self):
+        self.rec.listen(trigger=self.name)
 
     def run(self):
         while True:
-            name = self.config["Assistant"]["Name"] or "bob"
-            answer = bool(self.config["Assistant"]["Answer"] or False)
-            self.rec.listen(trigger=name or "bob")
+            self.wait_for_activation()
             play_beep()
+            promt = self.get_prompt()
 
-            promt = self.rec.listen()["transcription"]
-            print(f"Prompt: {promt}")
+            id, state, success = parse_cmd(promt)
+            if success:
+                self.tool.set_light_status(id, state)
+                continue
 
             bot = Bot().add_context_queue(self.c_queue).add_tool(self.tool)
             bot.add_message("user", promt)
             self.c_queue.add(Context("user", promt))
 
             response = bot.run()
-            if answer:
+            if self.answer:
                 speak(response.content)
             print(f"Response: {response}")
